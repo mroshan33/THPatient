db88a0d2c808e5322b7c86f4c6d2f75648159e7c
Roshan Sapkota <appleroshan8@gmail.com>
2021-04-01 22:43:02 +1100

    added all the files

---
+++ JS/1oauth.js
@@ -0,0 +1,57 @@
+// Based on https://github.com/andreassolberg/jso/tree/version3
+
+var ROOTURL = "localhost/Thv2";
+const RESTROOT = ROOTURL + "/wp-json";
+const RESTROUTE = RESTROOT + "/wp/v2/prescriptions/";
+
+var jso = new JSO({
+  providerID: "THPatient",
+  client_id: "W5AjiiAG9yDDqbsa9hKzpwb6j2j19RFmFdn9yK1l",
+  redirect_uri: "localhost/thv2/THPatient/prescriptionlist.html",
+  authorization: ROOTURL + "/oauth/authorize",
+});
+
+// Catch the response after login:
+jso.callback();
+
+var token = localStorage.getItem("tokens-PrescriptionBook");
+
+// Trigger OAuth 2 authentication sequence:
+function oauthLogin() {
+  jso.getToken();
+}
+
+// Log out and wipe all memory of the session:
+function oauthLogout() {
+  jso.wipeTokens();
+}
+
+// Monitor the login button:
+$("#login").click(function () {
+  oauthLogin();
+});
+
+// Monitor the logout button:
+$("#logout").click(function () {
+  oauthLogout();
+  window.location.href = "/";
+});
+
+(function () {
+  // If we are on the home page, redirect to tasklist.html:
+  if (location.pathname == "/") {
+    // If we have a token, assume we're logged in:
+    if (token !== null) {
+      window.location.href = "/prescriptionlist.html";
+    }
+  } else {
+    // If we have a token, assume we're logged in:
+    if (token !== null) {
+      // Enable JSO jQuery wrapper:
+      JSO.enablejQuery($);
+    } else {
+      // If we're not logged in, redirect to the login page:
+      window.location.href = "";
+    }
+  }
+})();
---
+++ JS/loadlist.ajax.js
@@ -0,0 +1,74 @@
+/**
+ * Script for loading the prescription list
+ * Constant RESTROUTE and variable token inherited from oath.js
+ */
+function getDate(object){
+  let date;
+  let options = {
+    weekday:  "long",
+    year:     "numeric",
+    month:    "short",
+    day:      "numeric",
+    hour:     "2-digit",
+    minute:   "2-digit",
+  };
+  let prescriptionDate = new Date(object.date);
+  date = 'Prescribed on <time datetime="' + object.date +'">' + prescriptionDate.toLocaleDateString("en-au", options) +
+  '</time>';
+  return date;
+}
+
+function createPrescriptionList(object) {
+  $(".prescription-list").empty().append("<ul></ul>");
+  
+  if (object.length!==0) {
+    $(".site-nav ul li p").empty().append("Your Prescriptions:");
+  for (let i = 0; i < object.length; i++) {
+    let navListItem =
+      '<li>' + 
+      '<a href="single.html?prescription=' + object[i].id + '">' +
+    '<h2 class="prescription-title">' + object[i].title.rendered + '</h2>' +
+    '<div class="prescription-date">' +
+      getDate(object[i]) +
+    '</div>'+
+
+    '</a>'+
+    '</li>';
+    $(".prescription-list ul").append(navListItem);
+  }
+  console.info(object);
+}else {
+    $(".site-nav ul li p").empty().append("You do not have any prescriptions yet.");
+
+  }
+}
+
+function getPrescriptionList() {
+  $(".prescription-list").append(
+    '<div class="loader"><img src="JS/spinner.svg" class="ajax-loader"/></div>'
+  );
+
+  jso
+    .ajax({
+      dataType: "json",
+      url: RESTROUTE,
+    })
+
+    .done(function (object) {
+      createPrescriptionList(object);
+    })
+
+    .fail(function () {
+      console.error("REST error. Nothing returned for AJAX.");
+    })
+
+    .always(function () {
+      $(".loader").remove();
+    });
+}
+
+if (token !== null) {
+  getPrescriptionList();
+} else {
+  window.location.href = "/";
+}
